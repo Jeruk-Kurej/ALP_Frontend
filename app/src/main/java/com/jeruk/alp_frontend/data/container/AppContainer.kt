36f3a562c2b19b6c1388bf7c52a6024ba30610b9
Bryan Carlie Lukito Setiawan <blukitosetiawan@gmail.com>
2026-01-06 22:25:32 +0700

    OMG KE DEPLOY

--- app/src/main/java/com/jeruk/alp_frontend/data/container/AppContainer.kt
+++ app/src/main/java/com/jeruk/alp_frontend/data/container/AppContainer.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object AppContainer {
     // Use 10.0.2.2 for Android Emulator (points to host machine's localhost)
     // Use 10.0.187.183 for Physical Device (your Mac's actual IP on network)
-    private const val ROOT_URL = "http://10.22.203.164:3000"
+    private const val ROOT_URL = "https://alpbackend-production.up.railway.app"
     private const val BASE_URL = "$ROOT_URL/api/"
 
     private val retrofit = Retrofit.Builder()
